cc945a121b02f4163e78ef5f4f66f8bd187c5359
NiShITa-code <75923914+NiShITa-code@users.noreply.github.com>
2023-06-07 11:52:51 +0530

    Added morris trvaersal

--- Binary Tree/inorder_traversal.cpp
+++ Binary Tree/inorder_traversal.cpp
@@ -29,18 +29,46 @@ public:
         stack <TreeNode * > s;
         TreeNode* curr = root;
         while (true) {
-        if (curr != NULL) {
-            s.push(curr);
-            curr = curr -> left;
-        } else {
-            if (s.empty()) break;
-            curr = s.top();
-            inorder.push_back(curr -> val);
-            s.pop();
-            curr = curr -> right;
-        }
+            if (curr != NULL) {
+                s.push(curr);
+                curr = curr -> left;
+            } else {
+                if (s.empty()) break;
+                curr = s.top();
+                inorder.push_back(curr -> val);
+                s.pop();
+                curr = curr -> right;
+            }
         }
         return inorder;
+        //Moris Iterative traversal
+       /* vector<int> inorder;
+        TreeNode* curr = root;
+        while(curr != NULL) {
+            if (curr -> left == NULL) {
+                // store the node itself because left does not exist
+                inorder.push_back(curr -> val);
+                // move to right
+                curr = curr -> right;
+            } else {
+                TreeNode* prev = curr -> left;
+                // find the rightmost node in left subtree
+                while (prev -> right != NULL && prev -> right != curr) {
+                    prev = prev -> right;
+                }
+
+                if (prev -> right == NULL) {
+                    prev -> right = curr;
+                    curr = curr -> left;
+                } else if (prev -> right == curr) {
+                    prev -> right = NULL;
+                    // push the root node
+                    inorder.push_back(curr -> val);
+                    curr = curr -> right;
+                }
+            }
+        }
+        return inorder;*/
     }
    
 };
